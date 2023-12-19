51721efd3c816ecf42bb6d20e751638914034456
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2023-12-19 16:44:39 +0530

    sign in page and next auth added

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,28 +3,76 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import { error } from 'console';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
+  customerId: z.string({ invalid_type_error: 'Please select a customer.' }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
   status: z.enum(['pending', 'paid']),
   date: z.string(),
 });
 
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoices(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-  //convert ammount to cents
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  //prepare data for insertion into the database
+
+  const { customerId, amount, status } = validatedFields.data;
+
+  // convert amount in cents
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
+
+  //insert data into database
 
   try {
     await sql`
@@ -32,6 +80,7 @@ export async function createInvoices(formData: FormData) {
   VALUES (${customerId},${amountInCents},${status},${date})
   `;
   } catch (error) {
+    // if database error occrus, return a more specific error
     return { message: 'failed to insert data' };
   }
   revalidatePath('/dashboard/invoices');
@@ -43,14 +92,30 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 // ...
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  // prepare data for mutate data
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
+
+  //mutate the data
 
   try {
     await sql`
