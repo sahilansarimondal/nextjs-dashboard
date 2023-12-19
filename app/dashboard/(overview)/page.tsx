8cb74a0151096771c078eb363c65c4f4ea357646
Sahil Ansari Mondal <mondalrajesh761@gmail.com>
2023-12-19 20:18:46 +0530

    adding metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,6 +2,11 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 import {
   RevenueChartSkeleton,
